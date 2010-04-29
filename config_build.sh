02d474724636d87c5f974deef822722f90b5daa5
Eotect Nahn <eotect@gmail.com>
2010-04-29 22:39:50 +0000

    add autobuild scripts
    git-svn-id: https://websaver2.googlecode.com/svn/trunk@2 29f5818d-08dc-29d4-2d60-6d16935589e1

---
+++ config_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build config for the build script, build.sh. Look there for more info.
+
+APP_NAME=websaver2
+CHROME_PROVIDERS="content locale skin"
+CLEAN_UP=1
+ROOT_FILES="Changes"
+ROOT_DIRS="defaults"
+BEFORE_BUILD=
+AFTER_BUILD=
